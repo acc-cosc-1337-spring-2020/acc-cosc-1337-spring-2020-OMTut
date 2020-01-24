cc90d3f661ddc70caa5aa2174cf4055f5d5f2bb0
Jason Tuttle <ron.tuttle@g.austincc.edu>
2020-01-23 19:03:16 -0600

    show include differences

--- src/examples/01_module/01_output/main.cpp
+++ src/examples/01_module/01_output/main.cpp
@@ -1,9 +1,11 @@
-#include "output.h"
+#include "output.h"//uses file code
+#include<iostream>//use standard library
 
+using std::cout;
 
 int main() 
 {
-	
+	cout <<"Hello World!";
 
 	return 0;
 }
