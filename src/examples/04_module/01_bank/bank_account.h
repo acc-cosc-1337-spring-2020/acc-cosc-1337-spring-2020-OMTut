265769425646c643e5264c089f12721dc53c4f18
Jason Tuttle <ron.tuttle@g.austincc.edu>
2020-02-25 19:14:55 -0600

    Built BankAccount class with deposit and withdraw functions

--- src/examples/04_module/01_bank/bank_account.h
+++ src/examples/04_module/01_bank/bank_account.h
@@ -1,1 +1,11 @@
 //bank_account.h
+class BankAccount
+{
+public:
+	BankAccount(int b) : balance{ b } {} // this is a constructor
+	int get_balance()const { return balance; } //const keeps the entire function from being amended
+	void deposit(int amount);
+	void withdraw(int amount);
+private:
+	int balance;
+};
