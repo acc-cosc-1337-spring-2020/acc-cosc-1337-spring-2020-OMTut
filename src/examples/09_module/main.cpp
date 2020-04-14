ce776ea7c18421b225178fac20e40c5e55830d49
Jason Tuttle <ron.tuttle@g.austincc.edu>
2020-04-14 18:56:37 -0500

    added example for file save and reading from file

--- src/examples/09_module/main.cpp
+++ src/examples/09_module/main.cpp
@@ -1,6 +1,22 @@
+#include "temperature_data.h"
+#include<iostream>
 
 int main() 
 {
+	std::vector<Temperature> out_temps{ 
+		Temperature(1, 98.9), Temperature(2, 98.5), Temperature(3, 99),
+		Temperature(4, 90.9), Temperature(5, 93.5), Temperature(6, 99.6) };
+		
+	TemperatureData data;
+
+	data.save_temps(out_temps);
+
+	std::vector<Temperature> in_temps = data.get_temps();
+
+	for (auto temp : in_temps)
+	{
+		std::cout << temp.get_hour() << " " << temp.get_reading() << "\n";
+	}
 
 	return 0;
 }
