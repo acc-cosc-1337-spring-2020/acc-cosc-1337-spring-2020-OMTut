a531f98ee2d02294ab74b30333999e56a4105f40
Jason Tuttle <ron.tuttle@g.austincc.edu>
2020-04-16 18:41:18 -0500

    Refernce review, intro to pointers

--- src/examples/11_module/01_ref_pointers/main.cpp
+++ src/examples/11_module/01_ref_pointers/main.cpp
@@ -1,8 +1,33 @@
 #include "ref_pointers.h"
 #include<iostream>
 
+using std::cout;
+
 int main() 
 {
+	int r = 0, p = 0;
+	int& ref_r = r; // this reference does not get its own address spot and modifies int r
 	
+
+	cout << "Address of r: " << &r << "\n";
+	cout << "Value of r: " << r << "\n";
+	cout << "Address of ref_r: " << &ref_r << "\n\n";
+
+	int* ptr_p = &p;
+	cout << "Address of p: " << &p << "\n";
+	cout << "Value of p: " << p << "\n";
+	cout << "Address of ptr_p: " << &ptr_p << "\n\n";
+
+	cout << "Contents(address) of ptr_p: " << ptr_p << "\n";
+	cout << "Value ptr_p points to: " << *ptr_p << "\n\n";
+
+	//THis changes the pointer
+	*ptr_p = 15;
+	cout << "Value ptr_p points to: " << *ptr_p << "\n";
+	cout << "Value of p: " << p << "\n\n";
+
+	//creating Dynamic Memory....Pass By Pointer
+
+
 	return 0;
 }
